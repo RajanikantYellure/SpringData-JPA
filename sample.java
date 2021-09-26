31edb07183c618ca43a8f3bc89018d00f5e410d9
rajanikantyellure <rajanikantyellure86@gmail.com>
2021-09-26 13:01:28 +0530

    Test class add Main method

--- sample.java
+++ sample.java
@@ -2,6 +2,10 @@ public class Test
 {
 public void main()
 {
-system.out.println("main method")
+system.out.println("main method");
 }
+public static void main(String[] args)
+{
+Test t=new Test();
+t.main();
 }
